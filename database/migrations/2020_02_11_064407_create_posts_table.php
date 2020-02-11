4c179b0ae986956756a006cc7e3931c3839ca9fe
yumauma20 <71yuma10@gmail.com>
2020-02-11 16:30:21 +0900

    マイグレーション実行

---
+++ database/migrations/2020_02_11_064407_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('cat_id'); //ポストテーブルとカテゴリーテーブルの紐づけに利用
+            $table->text('content');
+            $table->unsignedInteger('comment_count'); //投稿に何件のコメントがついたのかカウント
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
